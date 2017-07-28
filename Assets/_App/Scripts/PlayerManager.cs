4858b7be484910678a7d5c1134c757c910901107
valentincognito <valentin.vannay@gmail.com>
2017-07-28 17:59:41 +0900

    Implement Weapon Firing Logic

---
+++ Assets/_App/Scripts/PlayerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public bool isArmed;
+    public GameObject activeWeapon;
+
+    private WeaponBehaviour weaponBehaviour;
+
+    void Start()
+    {
+        weaponBehaviour = activeWeapon.GetComponent<WeaponBehaviour>();
+    }
+
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger))
+        {
+            if (isArmed)
+            {
+                weaponBehaviour.Shoot();
+            }
+        }
+    }
+}
---
+++ Assets/_App/Scripts/Weapons/WeaponBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ammoStock;
+    [SerializeField]
+    private Transform spawnPoint;
+    [SerializeField]
+    private float velocity;
+
+    public void Shoot()
+    {
+        Debug.Log("shoot");
+    }
+}
